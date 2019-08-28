0f89843741468927af2eaaf75ddad33eacaaf11a
anusenthil <anukrithika22@gamil.com>
2019-08-28 20:31:46 +0530

    new code for two dimensional list

--- src/test/java/org/cucumber/proj1/FirstPro.java
+++ src/test/java/org/cucumber/proj1/FirstPro.java
@@ -14,7 +14,7 @@ import cucumber.api.java.en.When;
 
 public class FirstPro {
 	static WebDriver driver;
-	@Given("user should launch browser")
+	/*@Given("user should launch browser")
 	public void user_should_launch_browser() {
 		System.setProperty("webdriver.chrome.driver", "E:\\Selenium\\Day1\\CucumberProj\\drivers\\chromedriver1.exe");
 		driver=new ChromeDriver();
@@ -25,7 +25,7 @@ public class FirstPro {
 	@Given("user should click add customer link")
 	public void user_should_click_add_customer_link() {
 	    driver.findElement(By.xpath("(//a[text()='Add Customer'])[1]")).click();
-	}
+	}*/
 
 	/*@When("user provide valid input")
 	public void user_provide_valid_input() {
@@ -65,7 +65,7 @@ public class FirstPro {
     
 	}*/
 	//<One Dimensional Map>
-	@When("user provide valid input")
+	/*@When("user provide valid input")
 	public void user_provide_valid_input(io.cucumber.datatable.DataTable dataTable) {
 		Map<String, String>onedieMap = dataTable.asMap(String.class,String.class);
 		driver.findElement(By.xpath("//label[text()='Done']")).click();
@@ -83,6 +83,40 @@ public class FirstPro {
 	public void to_verify_the_customer_id_is_displayed() {
 	    Assert.assertTrue(driver.findElement(By.xpath("(//td[@align='center'])[2]")).isDisplayed());
 	}
+*/
+	@Given("user should launch the browser")
+	public void user_should_launch_the_browser() {
+		System.setProperty("webdriver.chrome.driver", "E:\\Selenium\\Day1\\CucumberProj\\drivers\\chromedriver1.exe");
+		driver=new ChromeDriver();
+		driver.get("http://demo.guru99.com/telecom/");
+		driver.manage().window().maximize();
+	}
+
+	@Given("user should clik add tariff plan")
+	public void user_should_clik_add_tariff_plan() {
+		  driver.findElement(By.xpath("//a[text()='Add Tariff Plan']")).click();
+
+	}
+
+	@When("user provide valid input and sumbit")
+	public void user_provide_valid_input_and_sumbit(io.cucumber.datatable.DataTable dataTable) {
+		List<List<String>> twodimen = dataTable.asLists(String.class);
+		driver.findElement(By.id("rental1")).sendKeys(twodimen.get(0).get(0));
+	    driver.findElement(By.id("local_minutes")).sendKeys(twodimen.get(0).get(1));
+	    driver.findElement(By.id("inter_minutes")).sendKeys(twodimen.get(0).get(2));
+	    driver.findElement(By.id("sms_pack")).sendKeys(twodimen.get(1).get(3));
+	    driver.findElement(By.id("minutes_charges")).sendKeys(twodimen.get(1).get(4));
+	    driver.findElement(By.id("inter_charges")).sendKeys(twodimen.get(0).get(5));
+	    driver.findElement(By.id("sms_charges")).sendKeys(twodimen.get(2).get(6));
+	    driver.findElement(By.xpath("//input[@name='submit']")).click();
+	    }
+
+	@Then("to verify success message is displayed")
+	public void to_verify_success_message_is_displayed() {
+		Assert.assertTrue(driver.findElement(By.xpath("//h2[text()='Congratulation you add Tariff Plan']")).isDisplayed());
+	    
+	}
+
 
 
 }
---
+++ target/test-classes/Feature/twodList.feature
@@ -0,0 +1,13 @@
+@tag
+Feature: Add Tariff Plan
+  I want to use this template for my feature file
+
+  @tag1
+  Scenario: Getting successfully add message
+    Given user should launch the browser
+    And user should clik add tariff plan
+    When user provide valid input and sumbit
+      | 500 | 10000 |  5000 | 300 | 1 | 10 | 1 |
+      | 399 |  1000 |  1000 | 100 | 2 | 15 | 2 |
+      | 599 |  5000 | 10000 | 250 | 2 |  9 | 3 |
+    Then to verify success message is displayed
